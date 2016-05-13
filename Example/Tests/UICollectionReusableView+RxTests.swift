f5323ffab85ff194a29542a0a48385fdd23ebffb
sergdort <sergdort@gmail.com>
2016-05-14 00:14:29 +0300

    Added some DRY refactoring and tests

---
+++ Example/Tests/UICollectionReusableView+RxTests.swift
@@ -0,0 +1,38 @@
+//
+//  UICollectionReusableView+RxTests.swift
+//  Cell-Rx
+//
+//  Created by Segii Shulga on 5/13/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import XCTest
+import UIKit
+import RxSwift
+import Cell_Rx
+
+class UICollectionReusableView_RxTests: XCTestCase {
+
+    func test_that_it_has_dispose_bag_by_default() {
+        let view = UICollectionReusableView(frame: CGRect.zero)
+        
+        XCTAssertNotNil(view.rx_reusableDisposeBag)
+    }
+    
+    func test_that_it_returns_set_dispose_baag() {
+        let view = UICollectionReusableView(frame: CGRect.zero)
+        let disposeBag = DisposeBag()
+        view.rx_reusableDisposeBag = disposeBag
+        
+        XCTAssertTrue(view.rx_reusableDisposeBag === disposeBag)
+    }
+    
+    func test_that_it_changes_dispose_bag_on_reuse() {
+        let view = UICollectionReusableView(frame: CGRect.zero)
+        let disposeBag = DisposeBag()
+        view.rx_reusableDisposeBag = disposeBag
+        view.prepareForReuse()
+        
+        XCTAssertTrue(view.rx_reusableDisposeBag !== disposeBag)
+    }
+}
